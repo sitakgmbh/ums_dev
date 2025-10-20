57a80d0c21c81ce0b31b196546f347f237657721
Patrik Senti <patrik@sitak.ch>
2025-10-20 21:50:36 +0200

    1.1.0

--- resources/views/livewire/pages/admin/users/index.blade.php
+++ resources/views/livewire/pages/admin/users/index.blade.php
@@ -1,13 +1,15 @@
 <div>
-	@section('pageActions')
-		<a href="{{ route('admin.users.create') }}" class="btn btn-primary" title="Benutzer erstellen">
-			<i class="mdi mdi-account-plus"></i>
-		</a>
+	@section("pageActions")
+		@if (config("auth.mode", env("AUTH_MODE")) === "local")
+			<a href="{{ route("admin.users.create") }}" class="btn btn-primary" title="Benutzer erstellen">
+				<i class="mdi mdi-account-plus"></i>
+			</a>
+		@endif
 	@endsection
 
     <livewire:components.tables.users-table />
 </div>
 
-@section('modals')
+@section("modals")
     <livewire:components.modals.user-delete />
 @endsection
