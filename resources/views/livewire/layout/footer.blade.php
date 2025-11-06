546c315d7bbc5210bce7a156eb9e4adbd9fc5d7f
Patrik Senti <patrik@sitak.ch>
2025-11-06 18:46:27 +0100

    1.1.0

--- resources/views/livewire/layout/footer.blade.php
+++ resources/views/livewire/layout/footer.blade.php
@@ -7,11 +7,32 @@
             </div>
             <div class="col-md-6">
                 <div class="text-md-end footer-links d-none d-md-block">
-                    <a href="#">Hilfe</a>
-					<a href="#">Kontakt</a>
+                    <a href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvasHelp">Kontakt</a>
                 </div>
             </div>
         </div>
     </div>
 </footer>
 <!-- end Footer -->
+
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasHelp" aria-labelledby="offcanvasHelpLabel">
+    <div class="offcanvas-header">
+        <h3 id="offcanvasHelpLabel">Kontakt</h3>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+		<p class="mb-2">Bei Fragen oder Problemen wende dich bitte an:</p>
+		<!-- Kontakt -->
+		<div class="border rounded p-3 bg-light mt-1 shadow-sm">
+			<div class="d-flex align-items-center">
+				<span><strong>PDGR ICT Servicedesk</strong></span>
+			</div>
+			<div class="d-flex align-items-center">
+				<a href="tel:+41582252799" class="text-decoration-none link-secondary">+41 58 225 27 99</a>
+			</div>
+			<div class="d-flex align-items-center">
+				<a href="mailto:servicedesk@pdgr.ch" class="text-decoration-none link-secondary">servicedesk@pdgr.ch</a>
+			</div>
+		</div>
+    </div>
+</div>
