6e0320588476c17378f1ed52c451bab562ad8d2c
Patrik Senti <patrik@sitak.ch>
2025-11-11 10:00:31 +0100

    1.1.0

--- routes/console.php
+++ routes/console.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command("ad:sync-users")->hourly();
-Schedule::command("sap:sync")->dailyAt("01:05");
-Schedule::command("check:sap-ad")->dailyAt("01:10");
-Schedule::command("mypdgr:sync")->dailyAt("01:15");
-Schedule::command("eroeffnungen:assign-license")->dailyAt("01:20");
+Schedule::command("sap:sync")->dailyAt("23:05");
+Schedule::command("check:sap-ad")->dailyAt("23:10");
+Schedule::command("mypdgr:sync")->dailyAt("23:15");
+Schedule::command("eroeffnungen:assign-license")->dailyAt("23:20");
