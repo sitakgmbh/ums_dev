c8efe730260f898126470fcf451d5970a850d9a6
Patrik Senti <patrik@sitak.ch>
2025-12-31 10:51:58 +0100

    1.1.0

--- config/ums/feiertage.php
+++ config/ums/feiertage.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    // 2026
     "2026-01-01", // Neujahr
     "2026-01-02", // Berchtoldstag
     "2026-04-03", // Karfreitag
@@ -9,4 +10,14 @@ return [
     "2026-05-25", // Pfingstmontag
     "2026-08-01", // Nationalfeiertag
     "2026-12-25", // Weihnachten
+
+    // 2027
+    "2027-01-01", // Neujahr
+    "2027-01-02", // Berchtoldstag
+    "2027-03-26", // Karfreitag
+    "2027-03-29", // Ostermontag
+    "2027-05-06", // Auffahrt
+    "2027-05-17", // Pfingstmontag
+    "2027-08-01", // Nationalfeiertag
+    "2027-12-25", // Weihnachten
 ];
